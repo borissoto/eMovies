e25277eb1327978203169c5bfa94e06637faee05
Boris <borissoto@gmail.com>
2022-08-15 21:48:56 -0400

    Recyclerviews
    Recyclerview, top rated, upcoming, and recommended movies

--- app/src/main/java/com/boris/emovies/DetailFragment.kt
+++ app/src/main/java/com/boris/emovies/DetailFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.boris.emovies.databinding.FragmentDetailBinding
+import com.boris.emovies.databinding.FragmentMovieBinding
 
-class DetailFragment : Fragment(R.layout.fragment_movie) {
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailBinding.bind(view)
     }
 }
--- app/src/main/java/com/boris/emovies/MovieFragment.kt
+++ app/src/main/java/com/boris/emovies/MovieFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.boris.emovies.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 }
